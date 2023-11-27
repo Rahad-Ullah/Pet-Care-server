d43d4a1da575c2a7b038760b70fcfd75ba51648e
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-11-28 00:03:21 +0600

    Getting unadopted pets with category and search filtering has been implemented

--- index.js
+++ index.js
@@ -28,6 +28,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("petCareDB");
+    const petCollection = database.collection("pets")
+
+    // get pets waiting for adopt
+    app.get('/pets', async (req, res) => {
+        const category  = req.query.category;
+        const searchValue = req.query.search; 
+        console.log(category, searchValue)  
+        const query = {
+            category: category,
+            adopted: false,
+            name: {$regex: searchValue, $options: 'i'}
+        }
+        const options = {
+            sort: { adoption_date: -1, doption_time: -1 },
+        }
+        const result = await petCollection.find(query, options).toArray()
+        res.send(result)
+    })
 
 
 
